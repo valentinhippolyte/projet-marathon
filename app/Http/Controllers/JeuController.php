bf60a8dbd143d5d1bdf2fa398fcfc32378a1e438
Barbier Louis <louis_barbier@ens.univ-artois.fr>
2020-12-16 17:18:34 +0100

    create mise a jour

--- app/Http/Controllers/JeuController.php
+++ app/Http/Controllers/JeuController.php
@@ -27,11 +27,41 @@ class JeuController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
-    {
-        //
+    public function create() {
+        return view('jeux.create');
     }
 
+    public function store(Request $request){
+        // validation des données de la requête
+        $this->validate(
+            $request,
+            [
+                'nom' => 'required',
+                'description' => 'required',
+                'theme' => 'required',
+                'editeur' => 'required',
+            ]
+        );
+
+        // code exécuté uniquement si les données sont validaées
+        // sinon un message d'erreur est renvoyé vers l'utilisateur
+
+        // préparation de l'enregistrement à stocker dans la base de données
+        $jeux = new Jeu();
+
+
+        $jeux->nom = $request->nom;
+        $jeux->description = $request->description;
+        $jeux->theme_id = 1;
+        $jeux->editeur_id = 1;
+        $jeux->user_id = 1;
+
+        // insertion de l'enregistrement dans la base de données
+        $jeux->save();
+
+        // redirection vers la page qui affiche la liste des tâches
+        return redirect('/jeux');
+    }
 
 
     /**
