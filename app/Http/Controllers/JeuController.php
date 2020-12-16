d68676b8aa1010fa3752a38741df4a7df194f1bc
Matthieu Goliot <matthieugoliot@gmail.com>
2020-12-16 15:42:36 +0100

    Commit...

--- app/Http/Controllers/JeuController.php
+++ app/Http/Controllers/JeuController.php
@@ -42,66 +42,9 @@ class JeuController extends Controller
         return view('welcome', ['jeux' => $jeux]);
     }
 
-    function regles(){
+    function regles()
+    {
         $jeux = Jeu::all();
         return view('regles', ['jeux' => $jeux]);
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-     
     }
 }
